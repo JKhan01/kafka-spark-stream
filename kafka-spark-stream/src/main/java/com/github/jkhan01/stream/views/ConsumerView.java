f010aa59469486765d388bef7188a3cab11fb379
Jamaluddin Khan <jkhan266@gmail.com>
2021-11-02 15:58:47 +0530

    Continuous Producer Consumer Streaming Tested. Data Successfully Getting Pushed to CSV.

--- kafka-spark-stream/src/main/java/com/github/jkhan01/stream/views/ConsumerView.java
+++ kafka-spark-stream/src/main/java/com/github/jkhan01/stream/views/ConsumerView.java
@@ -1,5 +1,49 @@
 package com.github.jkhan01.stream.views;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.github.jkhan01.kafka.clients.ConsumerClient;
+
 public class ConsumerView {
+	
+	public static void printToCSV(ConsumerRecord< String, String> record) throws ParseException, IOException {
+		JSONParser parser = new JSONParser();  
+		JSONObject json = (JSONObject) parser.parse(record.value());
+		
+		
+		FileWriter csvWriter = new FileWriter("F:\\BigDataTraining\\Assignment_Kafka_Spark\\kafka-spark-stream\\data-csv\\sensor-data.csv", true);
+		
+		csvWriter.append(json.get("timestamp").toString()+",");
+		csvWriter.append(json.get("uniqueId").toString()+",");
+		csvWriter.append(json.get("floorNumber")+",");
+		csvWriter.append(json.get("sensorId")+",");
+		csvWriter.append(json.get("temperature").toString() + "\n");
+		
+		csvWriter.flush();
+		csvWriter.close();
+		
+	}
+	
+	public static void main(String[] args) {
+		ConsumerClient client = new ConsumerClient();
+		while (true) {
+			for (ConsumerRecord<String, String> record: client.fetchDataFromServer()) {
+				System.out.println("Fetched Message: " + record.value());
+				try {
+					ConsumerView.printToCSV(record);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					System.out.println(e.getMessage());
+				}
+			}	
+		}
+		
+	}
 
 }
--- kafka-spark-stream/src/main/java/com/github/jkhan01/stream/views/ProducerView.java
+++ kafka-spark-stream/src/main/java/com/github/jkhan01/stream/views/ProducerView.java
@@ -1,5 +1,38 @@
 package com.github.jkhan01.stream.views;
 
+
+import java.util.concurrent.TimeUnit;
+
+
+
+import com.github.jkhan01.kafka.clients.ProducerClient;
+import com.github.jkhan01.kafka.constants.ApplicationConstants;
+
 public class ProducerView {
+	
+	public static void main(String[] args) {
+		ProducerClient client = new ProducerClient();
+
+		TimeUnit timer = TimeUnit.MILLISECONDS;
+
+		
+		while(true) {
+			try {
+				client.publishSensorData();
+
+				timer.sleep(ApplicationConstants.getPUBLISHING_INTERVAL_IN_MILLIS());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+
+			}
+		}
+		
+		
+		
+
+		
+		
+	}
 
 }
